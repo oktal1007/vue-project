5ad99a74201da0acacd93835372bfdd8f8185da0
oktal <thelegendofzelda107@gmail.com>
2019-06-19 20:00:33 +0900

    test

--- config/index.js
+++ config/index.js
@@ -48,9 +48,9 @@ module.exports = {
     index: path.resolve(__dirname, '../docs/index.html'),
 
     // Paths
-    assetsRoot: path.resolve(__dirname, '../docs'),
+    assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: './',
+    assetsPublicPath: '',
 
     /**
      * Source Maps
